de1ef13bc310a057d14104ecba879cb1d9d9689b
dilnakt-bnglr <dilna.devaraj98@gmail.com>
2026-05-28 23:27:18 +0530

    created router and header component

---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/435e8bb8-7f1b-49cb-8da8-bff997124294/web/IN-en-20260511-TRIFECTA-perspective_ec39852e-0b48-4e8a-b415-dd8376cd83ce_medium.jpg"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
